5116abfec831330bc944163deed580c2b2d03ca0
Davorin <ddoung@sfu.ca>
2018-09-24 20:21:00 -0700

    Create Post model and test

--- backend/posts/tests.py
+++ backend/posts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(title='first post')
+        Post.objects.create(description='post description here')
+
+    def test_title_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.title}'
+        self.assertEquals(expected_object_name, 'first post')
+
+    def test_description_content(self):
+        post = Post.objects.get(id=2)
+        expected_object_name = f'{post.description}'
+        print(expected_object_name)
+        self.assertEquals(expected_object_name, 'post description here')
