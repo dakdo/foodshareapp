38548efea306836720f68b22c38bdc5d39c50ae3
Davorin <ddoung@sfu.ca>
2018-09-30 15:00:00 -0700

    Add created_by and pub_date properties to posts and create unique url endpoint

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListPost.as_view()),
-    path('<int:pk>/', views.DetailPost.as_view()),
+    path('posts/', views.ListPost.as_view()),
+    path('posts/<int:pk>/', views.DetailPost.as_view()),
 ]
